7bd2664712589b989ac7aa6a22542f981d1ba472
tup <2363176358@qq.com>
2023-09-19 23:53:46 +0800

    add:

--- test/test01/test01/ContentView.swift
+++ test/test01/test01/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+     
         VStack {
+            
+            Image("icon").resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 10.0, trailing: 0.0)).imageScale(.small)
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello 222")
         }
         .padding()
@@ -22,5 +27,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(width: nil)
     }
 }
